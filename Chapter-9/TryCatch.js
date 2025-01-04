22b323cfda9bb1b32e78be3ac87d8782ac1ed95a
AI-3Tech <injalacharya1@gmail.com>
2025-01-04 22:56:34 +0545

    Try and Catch

---
+++ Chapter-9/TryCatch.js
@@ -0,0 +1,27 @@
+setTimeout(() => {
+    console.log("Hacking your system....")
+}, 1000)
+// setTimeout(() => {
+//     try {
+//     console.log(Rahul)
+// }
+// catch(error) {
+//     console.log("Error occured " + error)
+// }
+// }, 2000)
+
+try {
+    console.log(Hehehehe)
+}
+
+catch(heheh) {          //any name can be given to the error caught by the the catch... statement E.g here heheh is given as the error name
+    console.log("An error has occured " + heheh)
+}
+
+setTimeout(() => {
+    console.log("Please wait...")
+}, 3000)
+
+setTimeout (() => {
+    console.log("System compromised successfully ('.')")
+}, 5000)
