4ed2b4f8e49dad1cdb4ccf3b4cdf9e68127c0b83
AI-3Tech <injalacharya1@gmail.com>
2025-01-05 21:59:57 +0545

    Chapter-9 Practice Questions Done

--- Chapter-9/PracticeQs.js
+++ Chapter-9/PracticeQs.js
@@ -52,3 +52,42 @@ const loadscript = async (src) => {
 // }
 // b()
 
+//Problem -4 Solution
+let p1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(10)
+        }, 1000)
+    })
+}
+let p2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(20)
+        }, 2000)
+    })
+}
+let p3 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(30)
+        }, 3000)
+    })
+}
+
+let a = async () => {
+    console.time("run: ")
+    // let a1 = await p1()
+    // let a2 = await p2()
+    // let a3 = await p3()
+    
+
+    let a1 = p1()
+    let a2 = p2()
+    let a3 = p3()
+    let a1a2a3 = await Promise.all([a1, a2, a3])
+    console.log(a1a2a3)
+    console.timeEnd("run: ")
+}
+
+a()
