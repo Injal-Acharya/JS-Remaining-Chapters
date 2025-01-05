7f345183597e438a8b30772f331e0a90dfabec52
AI-3Tech <injalacharya1@gmail.com>
2025-01-05 21:23:55 +0545

    Practice Question-3 done

--- Chapter-9/PracticeQs.js
+++ Chapter-9/PracticeQs.js
@@ -12,12 +12,43 @@ const loadscript = async (src) => {
     })
 }
 
-let a = loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")
-a.then((value) => {
-    console.log(value)
-    alert("Script loaded successfully")
-})
+//Problem -1 Solution 
 
-.catch((error) => {
-    console.log(error)
-})
+// let a = loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")
+// a.then((value) => {
+//     console.log(value)
+//     alert("Script loaded successfully")
+// })
+
+// .catch((error) => {
+//     console.log(error)
+// })
+
+//Problem -2 Solution 
+// let b = async () => {
+//     let c = await loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")   
+//     alert("Script loaded successfully") 
+// }
+
+// b()
+
+//Problem -3 Solution
+// let a = async () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout (() => {
+//             reject("This is not accepted")
+//         }, 3000)
+//     })
+// }
+
+// let b = async () => {
+//     try {
+//         let c = await a()
+//         console.log(c)
+//     }
+//     catch(error) {
+//         console.log("Error: " + error)
+//     }
+// }
+// b()
+
