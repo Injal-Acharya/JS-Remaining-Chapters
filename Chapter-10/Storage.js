b76ece3e1a0b456573da4c8f6c94004e915d0eec
AI-3Tech <injalacharya1@gmail.com>
2025-01-17 23:28:35 +0545

    Storage Events completed

--- Chapter-10/Storage.js
+++ Chapter-10/Storage.js
@@ -18,6 +18,6 @@ console.log(sessionStorage.getItem(key))
 
 //Storage events
 window.onstorage = (event) => {
-    alert(`Value changed in the storage`)
+    alert(`Value changed in storage`)
     console.log(event)
 }
